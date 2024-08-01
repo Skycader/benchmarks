6e70feb7581cbe6af9e06a9c1c64bc9cb4f13db2
skycader <skycader@yandex.ru>
2024-08-01 11:13:19 +0300

    init

---
+++ my.js
@@ -0,0 +1,55 @@
+let s1 = [
+  "fregrew",
+  "kuitkyu",
+  "ygryjn",
+  "yjdhy",
+  "ndyytjyt",
+  "njytjrt",
+  "ifgkuyk",
+  "muffukuy",
+  "uyfk,fy,y",
+  "",
+];
+
+function getRandomString() {
+  return (
+    s1[Math.round(Math.random() * s1.length)] +
+    " " +
+    s1[Math.round(Math.random() * s1.length)]
+  );
+}
+
+function getRandomFlight() {
+  let f = {
+    flagPnr: Math.random() > 0.05,
+    routeNumber: Math.round(Math.random() * 1000).toString(),
+    planDepartDate: getRandomString(),
+    planDepartDateMs: 1721144400000 + Math.round(Math.random() * 10000000000),
+    departAirportNameRu: getRandomString(),
+    arriveAirportNameRu: getRandomString(),
+    passExists: Math.random() > 0.05 ? "true" : "false",
+    crewExists: Math.random() > 0.05 ? "true" : "false",
+  };
+  return f;
+}
+
+const array = [];
+for (let i = 0; i <= 1000000; i++) {
+  array.push(getRandomFlight());
+}
+
+const start = Date.now();
+
+const departMillisecondsMin = array.reduce((prev, curr) => {
+  const p = prev;
+  const c = curr;
+
+  if (c.planDepartDateMs && p.planDepartDateMs)
+    return p.planDepartDateMs < c.planDepartDateMs ? p : c;
+  return p;
+});
+
+const minDeparture = departMillisecondsMin;
+const minMs = minDeparture.planDepartDateMs || 0;
+
+console.log("DONE", Date.now() - start, "RESULT: ", minMs);
